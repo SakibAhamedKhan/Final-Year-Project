2a87d10baba440b5f34ac6c27142ef65d67dafc7
SakibAhamedKhan <Sakibahamedkhan01@gmail.com>
2023-05-20 21:29:22 +0600

    Side Navbar floating done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -8,29 +8,27 @@ import Navbar from '../shared/navbar';
 import UpperNavbar from '../shared/upperNavbar';
 import { Route, Router, Routes } from 'react-router-dom';
 
-
-
-
 function Home() {
     const [show, setShow] = useState(true);
-    
+    console.log(show);
+
     return (
         <div className='flex flex-row'>
-            <div className="basis-2/12 border-r-2 h-screen border-gray-200" >
-            <Navbar />
+            <div className={`${show? 'bg-blue-200': 'bg-white w-[50px]'} border-r-2 h-screen border-gray-2001`} >
+                <Navbar show={show}/>
             </div>
 
             <div className="w-full bg-gray-100 relative">
-            <div className='h-[40px] bg-white w-full drop-shadow-md absolute z-10'>
-                <UpperNavbar/>
-            </div>
-            <Routes >
-                <Route path='/' element={<Newsfeed></Newsfeed>}></Route>
-                <Route path='/papersearch' element={<PaperSearch></PaperSearch>}></Route>
-                <Route path='/paperpublish' element={<PaperPublish></PaperPublish>}></Route>
-                <Route path='/community' element={<Community></Community>}></Route>
-                <Route path='/profile' element={<Profile></Profile>}></Route>
-            </Routes>
+                <div className='h-[40px] bg-white w-full drop-shadow-md absolute z-10'>
+                    <UpperNavbar show={show} setShow={setShow}/>
+                </div>
+                <Routes >
+                    <Route path='/' element={<Newsfeed></Newsfeed>}></Route>
+                    <Route path='/papersearch' element={<PaperSearch></PaperSearch>}></Route>
+                    <Route path='/paperpublish' element={<PaperPublish></PaperPublish>}></Route>
+                    <Route path='/community' element={<Community></Community>}></Route>
+                    <Route path='/profile' element={<Profile></Profile>}></Route>
+                </Routes>
             </div>
         </div>
     );
--- src/pages/shared/navbar.js
+++ src/pages/shared/navbar.js
@@ -1,19 +1,44 @@
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import './navbar.css'
+import {BiNews} from 'react-icons/bi';
+import {MdOutlineManageSearch, MdPublishedWithChanges} from 'react-icons/md'
+import {FiHelpCircle} from 'react-icons/fi'
 
-function Navbar() {
+function Navbar(props) {
+    const {show} = props;
+
     let activeStyle = {
         textDecoration: "underline",
     };
 
     return (
-        <div className="px-8 pt-6">
+        <div className="px-4 pt-2">
             <ul className="flex flex-col items-center">
-                <li><NavLink to='/' className="btn btn-sm my-2 w-56 btn-outline rounded-[5px]">News Feed</NavLink></li>
-                <li><NavLink to='/papersearch' className="btn btn-sm my-2 w-56 btn-outline rounded-[5px]">Search Paper</NavLink></li>
-                <li><NavLink to='/paperpublish' className="btn btn-sm my-2 w-56 btn-outline rounded-[5px]">Publish Paper</NavLink></li>
-                <li><NavLink to='/community' className="btn btn-sm my-2 w-56 btn-outline rounded-[5px]">Community Help</NavLink></li>
+                <li><NavLink to='/' className={`btn btn-sm my-2 ${show? 'w-44': 'w-8'} btn-outline rounded-[5px] capitalize`}>
+                    <div className="flex flex-row jusity-start items-center">
+                        <div className={` ${show? 'pr-2':''}  text-lg`}><BiNews/></div>
+                        <p className={`${show ? '':'hidden'}`}>News Feed</p>
+                    </div>   
+                </NavLink></li>
+                <li><NavLink to='/papersearch' className={`btn btn-sm my-2 ${show? 'w-44': 'w-8'} btn-outline rounded-[5px] capitalize`}>
+                    <div className="flex flex-row jusity-start items-center">
+                        <div className={` ${show? 'pr-2':''}  text-lg`}><MdOutlineManageSearch/></div>
+                        <p className={`${show ? '':'hidden'}`}>Search Paper</p>
+                    </div>      
+                </NavLink></li>
+                <li><NavLink to='/paperpublish' className={`btn btn-sm my-2 ${show? 'w-44': 'w-8'} btn-outline rounded-[5px] capitalize`}>
+                    <div className="flex flex-row jusity-start items-center">
+                        <div className={` ${show? 'pr-2':''}  text-lg`}><MdPublishedWithChanges/></div>
+                        <p className={`${show ? '':'hidden'}`}>Publish Paper</p>
+                    </div>      
+                </NavLink></li>
+                <li><NavLink to='/community' className={`btn btn-sm my-2 ${show? 'w-44': 'w-8'} btn-outline rounded-[5px] capitalize`}>
+                    <div className="flex flex-row jusity-start items-center">
+                        <div className={` ${show? 'pr-2':''}  text-lg`}><FiHelpCircle/></div>
+                        <p className={`${show ? '':'hidden'}`}>Community Help</p>
+                    </div>     
+                </NavLink></li>
                 {/* <li><NavLink to='/profile' className="btn btn-sm my-2 w-56 btn-outline rounded-[5px]">Profile</NavLink></li> */}
             </ul>
         </div>
